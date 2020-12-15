38af76b451b97b89dac634c2a708be7754f66979
Mehmet Serkan Ekinci <mehmetserkanekinci@gmail.com>
2020-12-15 21:30:29 +0300

    generic interface and method added and DI extension class created

---
+++ MSEkinci.BasicBlog.Business/Concrete/GenericManager.cs
@@ -0,0 +1,59 @@
+﻿using MSEkinci.BasicBlog.Business.Interfaces;
+using MSESoftware.BasicBlog.DataAccess.Interfaces;
+using MSESoftware.BasicBlog.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MSEkinci.BasicBlog.Business.Concrete
+{
+    public class GenericManager<TEntity> : IGenericService<TEntity> where TEntity : class, IEntity, new()
+    {
+        private readonly IGenericDAL<TEntity> _genericDal;
+        public GenericManager(IGenericDAL<TEntity> genericDal)
+        {
+            _genericDal = genericDal;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _genericDal.AddAsync(entity);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _genericDal.GetAllAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _genericDal.GetAllAsync(filter);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TKey>> keySelector)
+        {
+            return await _genericDal.GetAllAsync(filter, keySelector);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> keySelector)
+        {
+            return await _genericDal.GetAllAsync(keySelector);
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _genericDal.GetAsync(filter);
+        }
+
+        public async Task RemoveAsync(TEntity entity)
+        {
+            await _genericDal.RemoveAsync(entity);
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            await _genericDal.UpdateAsync(entity);
+        }
+    }
+}
---
+++ MSEkinci.BasicBlog.Business/Containers/MicrosoftIOC/CustomIoCExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MSEkinci.BasicBlog.Business.Concrete;
+using MSEkinci.BasicBlog.Business.Interfaces;
+using MSESoftware.BasicBlog.DataAccess.Concrete.EntityFrameworkCore.Repositories;
+using MSESoftware.BasicBlog.DataAccess.Interfaces;
+
+namespace MSEkinci.BasicBlog.Business.Containers.MicrosoftIOC
+{
+    public static class CustomIoCExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericDAL<>), typeof(EFGenericRepository<>));
+            services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+        }
+    }
+}
