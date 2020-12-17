72696c3faef09d25a3a1040af261bb67aad13919
Mehmet Serkan Ekinci <mehmetserkanekinci@gmail.com>
2020-12-17 09:32:14 +0300

    template created for home and post , blog controller created, viewcomponent added for categories, needed css and js files added with libman

--- Frontend/BasicBlogFront/Controllers/HomeController.cs
+++ Frontend/BasicBlogFront/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BasicBlogFront.ApiServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace BasicBlogFront.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IBlogApiService _blogApiService;
+        public HomeController(IBlogApiService blogApiService)
         {
-            return View();
+            _blogApiService = blogApiService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _blogApiService.GetAllAsync());
+        }
+
+        public async Task<IActionResult> BlogDetail(int id)
+        {
+            return View(await _blogApiService.GetByIdAsync(id));
         }
     }
 }
--- Frontend/BasicBlogFront/Startup.cs
+++ Frontend/BasicBlogFront/Startup.cs
@@ -1,3 +1,5 @@
+using BasicBlogFront.ApiServices.Concrete;
+using BasicBlogFront.ApiServices.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +20,8 @@ namespace BasicBlogFront
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<IBlogApiService, BlogApiManager>();
+            services.AddHttpClient<ICategoryApiService, CategoryApiManager>();
             services.AddRazorPages();
         }
 
