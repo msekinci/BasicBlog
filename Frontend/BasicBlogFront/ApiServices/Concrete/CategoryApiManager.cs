7b13e800f1a1e9028d7c1c54d172e04c43ac6589
Mehmet Serkan Ekinci <mehmetserkanekinci@gmail.com>
2020-12-20 00:55:36 +0300

    filter blogs by category

--- Frontend/BasicBlogFront/ApiServices/Concrete/CategoryApiManager.cs
+++ Frontend/BasicBlogFront/ApiServices/Concrete/CategoryApiManager.cs
@@ -34,5 +34,15 @@ namespace BasicBlogFront.ApiServices.Concrete
             }
             return null;
         }
+
+        public async Task<CategoryListModel> GetByIdAsync(int id)
+        {
+            var responseMessage = await _httpClient.GetAsync($"{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<CategoryListModel>(await responseMessage.Content.ReadAsStringAsync());
+            }
+            return null;
+        }
     }
 }
---
+++ Frontend/BasicBlogFront/TagHelpers/CategoryFilterTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using BasicBlogFront.ApiServices.Interfaces;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace BasicBlogFront.TagHelpers
+{
+    [HtmlTargetElement("categoryfilter")]
+    public class CategoryFilterTagHelper : TagHelper
+    {
+        private readonly ICategoryApiService _categoryApiService;
+        public int? CategoryId { get; set; }
+
+        public CategoryFilterTagHelper(ICategoryApiService categoryApiService)
+        {
+            _categoryApiService = categoryApiService;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (CategoryId.HasValue)
+            {
+                var category = await _categoryApiService.GetByIdAsync(CategoryId.Value);
+                string html = $"<div class='border border-info p-3 mb-2'>{category.Name} blogs are listed." +
+                $"<a class='float-right' href='Home'>Remove Filter</a></div>";
+                output.Content.SetHtmlContent(html);
+            }
+        }
+    }
+}
